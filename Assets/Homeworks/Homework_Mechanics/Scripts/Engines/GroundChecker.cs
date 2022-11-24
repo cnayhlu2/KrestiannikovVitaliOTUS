641b58b085eb4ac5cb3e4f5f8fd3282899943d7e
cnayhlu2 <cnayhlu2@mail.ru>
2022-11-24 19:26:12 +0700

    fix gravity

--- Assets/Homeworks/Homework_Mechanics/Scripts/Engines/GroundChecker.cs
+++ Assets/Homeworks/Homework_Mechanics/Scripts/Engines/GroundChecker.cs
@@ -6,9 +6,9 @@ namespace Homework_Mechanics.Engines
     public class GroundChecker : MonoBehaviour
     {
         [SerializeField, Required] private TransformEngine transformEngine;
-        [SerializeField] private float groundHeight = 0f;
+        [SerializeField] private float distanceToGround = 0.01f;
 
-        public bool IsOnGround => transformEngine.Position.y <= groundHeight;
-        public float GetGroundHeight => groundHeight;
+        public bool IsOnGround => Physics.Raycast(transformEngine.Position, Vector3.down, distanceToGround);
+        public float GetGroundHeight => distanceToGround;
     }
 }
