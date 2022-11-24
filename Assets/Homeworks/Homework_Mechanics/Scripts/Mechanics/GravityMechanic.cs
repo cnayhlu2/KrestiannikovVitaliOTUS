c6c6bf4e741b569d9a3df80bbcab2ffcfe67a741
cnayhlu2 <cnayhlu2@mail.ru>
2022-11-24 22:23:22 +0700

    rework gravity

--- Assets/Homeworks/Homework_Mechanics/Scripts/Mechanics/GravityMechanic.cs
+++ Assets/Homeworks/Homework_Mechanics/Scripts/Mechanics/GravityMechanic.cs
@@ -1,22 +1,19 @@
-using Homework_Mechanics.Engines;
+﻿using Homework_Mechanics.Engines;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Homework_Mechanics.Mechanics
 {
-    public class AlwaysMoveDirectionMechanic : MonoBehaviour
+    public class GravityMechanic : MonoBehaviour
     {
         [SerializeField, Required] private TransformEngine positionEngine;
         [SerializeField, Required] private GroundChecker groundChecker;
-        [SerializeField] private Vector3 direction;
+        [SerializeField] private float force = 1f;
 
         private void FixedUpdate()
         {
             if (!groundChecker.IsOnGround)
-                positionEngine.Position += (direction * Time.deltaTime);
+                positionEngine.Position -= new Vector3(0, force * Time.deltaTime, 0);
         }
-
-
     }
 }
-
