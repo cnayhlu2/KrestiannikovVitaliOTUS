8a5f126fef73c11ed60e996667548fcbb6db4007
cnayhlu2 <cnayhlu2@mail.ru>
2022-12-13 16:11:06 +0700

    complite visual

---
+++ Assets/Homeworks/Homework_06_Conveyor/Scripts/ConveyorVisual.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Homework_06_Conveyor
+{
+    public class ConveyorVisual : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> items = new();
+        
+        
+        
+    }
+}
