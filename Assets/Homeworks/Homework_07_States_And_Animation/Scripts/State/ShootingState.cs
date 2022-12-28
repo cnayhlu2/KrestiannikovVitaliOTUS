823758c80b6f74d8cdf6188f3d9373f5a3b5b54e
cnayhlu2 <cnayhlu2@mail.ru>
2022-12-28 10:10:01 +0700

    shooting state

---
+++ Assets/Homeworks/Homework_07_States_And_Animation/Scripts/State/ShootingState.cs
@@ -0,0 +1,40 @@
+﻿using Homework_States.Animation;
+using UnityEngine;
+
+namespace Homework_States.State
+{
+    public class ShootingState : State
+    {
+        [SerializeField] private AnimationSystem animationSystem;
+
+        [SerializeField] private string createProjectileKey = "";
+
+        [SerializeField] private Transform attackPoint;
+        [SerializeField] private GameObject projectile;
+
+        public override void Enter()
+        {
+            this.animationSystem.OnEventReceived += OnEventReceived;
+        }
+
+
+        public override void Exit()
+        {
+            this.animationSystem.OnEventReceived += OnEventReceived;
+        }
+
+
+        private void OnEventReceived(string key)
+        {
+            if (createProjectileKey == key)
+            {
+                Shoot();
+            }
+        }
+
+        private void Shoot()
+        {
+            Instantiate(projectile, attackPoint.position, Quaternion.identity);
+        }
+    }
+}
